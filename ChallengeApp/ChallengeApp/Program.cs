c05a73bb54c0cf1a5a0676faa0971650273cc9f0
LA5ERman <laserman71@live.co.uk>
2024-01-18 11:52:53 +0000

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,3 +1,18 @@
-﻿Console.WriteLine("Hello Big World!");
-var bigNumber = float.MaxValue;
-Console.WriteLine("Big number is: " + bigNumber);
+﻿var name = "Ewa";
+char sex = 'm';
+var age = 30;
+
+if (age < 30 && sex == 'k')
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+
+else if (sex == 'm' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
